adbfc4211350831bf4eb5d6025d171d3d4f470d9
Diana <dianatabraue@gmail.com>
2023-08-21 11:57:23 +0100

    server done

--- server.js
+++ server.js
@@ -1,18 +1,44 @@
 'use strict';
 require('dotenv').config();
-const express = require('express');
-const myDB = require('./connection');
-const fccTesting = require('./freeCodeCamp/fcctesting.js');
+const express      = require('express');
+const myDB         = require('./connection');
+const fccTesting   = require('./freeCodeCamp/fcctesting.js');
+const passport     = require('passport');
+const session      = require('express-session')
+const { ObjectID } = require('mongodb');
 
-const app = express();
+
+const app = express(); 
 
 fccTesting(app); //For FCC testing purposes
 app.use('/public', express.static(process.cwd() + '/public'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-
+app.set('view engine', 'pug')
+app.set('views','./views/pug')
 app.route('/').get((req, res) => {
+  res.render('index', {
+    title: 'Hello',
+    message: 'Please log in'
+  })
+});
+app.use(passport.initialize())
+app.use(session({
+  secret: process.env.SESSION_SECRET,
+  resave: true,
+  saveUninitialized: true,
+  cookie: { secure: false }
+}));
+app.use(passport.session())
 
+passport.serializeUser((user, done) => {
+  done(null, user._id);
+});
+
+passport.deserializeUser((id, done) => {
+  //myDataBase.findOne({ _id: new ObjectID(id) }, (err, doc) => {
+    done(null, null);
+  //});
 });
 
 const PORT = process.env.PORT || 3000;
